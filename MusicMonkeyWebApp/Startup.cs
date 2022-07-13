d845c57341f626ec7383a1d57ebec2b41d64c0ad
kiriakos <kiris3992@gmail.com>
2022-07-13 22:14:25 +0300

    Solution Start

---
+++ MusicMonkeyWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicMonkeyWebApp.Startup))]
+namespace MusicMonkeyWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
