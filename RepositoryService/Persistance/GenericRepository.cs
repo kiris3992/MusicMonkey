131beb18d0556834cb330bcb227812b587b10f56
Panagiotis Chnarakis <panagiotis.chnarakis@Peoplecert.onmicrosoft.com>
2022-08-30 21:31:07 +0300

    repository changes

---
+++ RepositoryService/Persistance/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using Entities.Models;
+using RepositoryService.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryService.Persistance
+{
+    internal class GenericRepository<T> : IGenericRepository<T> where T : MusicEntity
+    {
+        
+        public void Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
