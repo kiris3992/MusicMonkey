c00caf42762ea1d9b83d22e0f1d85fa6bbf23e7f
kiriakos <kiris3992@gmail.com>
2022-09-05 22:22:52 +0300

    Seeding

--- DAL/Initializers/MockUpDbInitializer.cs
+++ DAL/Initializers/MockUpDbInitializer.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Entities.Models;
+using Entities.Enums;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity.Migrations;
 
 namespace DAL.Initializers
 {
@@ -11,7 +14,62 @@ namespace DAL.Initializers
     {
         protected override void Seed(ApplicationDbContext context)
         {
+            var artists = new List<Artist>
+            {
+                new Artist  // Nirvana
+                {
+                    Name = "Nirvana", Country = Country.USA, CareerStartDate = new DateTime(1987, 12, 1), PhotoUrl = "", ArtistGenres = new List<Genre> { Genre.Rock },
+                    Albums = new List<Album> {
+                        new Album {
+                            Title = "Nevermind", ReleaseDate = new DateTime(1991, 9, 4), CoverPhotoUrl = "https://lastfm.freetls.fastly.net/i/u/ar0/570021b68d3d9d2db08bc99a473303b0.jpg",
+                            Tracks = new List<Track> {
+                                new Track { Title = "Smells Like Teen Spirit", DurationSecs = 301, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Nirvana_-_Nevermind_-_1_-_Smells_Like_Teen_Spirit_(Official_Music_Video)_(320kbps).mp3" },
+                                new Track { Title = "In Bloom", DurationSecs = 255, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Nirvana_-_Nevermind_-_2_-_In_Bloom_(Official_Music_Video)_(320kbps).mp3" },
+                                new Track { Title = "Come as You Are", DurationSecs = 219, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Nirvana_-_Nevermind_-_3_-_Come_As_You_Are_(Official_Music_Video)_(320kbps).mp3" },
+                                new Track { Title = "Breed", DurationSecs = 184, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Nirvana_-_Nevermind_-_4_-_Breed_(Audio)_(320kbps).mp3" },
+                                new Track { Title = "Lithium", DurationSecs = 257, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Nirvana_-_Nevermind_-_5_-_Lithium,_LP_Version_(Lithium,_Single)_(320kbps).mp3" },
+                                new Track { Title = "Polly", DurationSecs = 177, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Nirvana_-_Nevermind_-_6_-_Polly_(Live_On_MTV_Unplugged,_1993___Unedited)_(320kbps).mp3" },
+                                new Track { Title = "Territorial Pissings", DurationSecs = 143, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Nirvana_-_Nevermind_-_7_-_Territorial_Pissings_(Audio)_(320kbps).mp3" },
+                                new Track { Title = "Drain You", DurationSecs = 224, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Nirvana_-_Nevermind_-_8_-_Drain_You_(Audio)_(320kbps).mp3" },
+                                new Track { Title = "Lounge Act", DurationSecs = 157, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Nirvana_-_Nevermind_-_9_-_Lounge_Act_(Audio)_(320kbps).mp3" },
+                                new Track { Title = "Stay Away", DurationSecs = 212, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Nirvana_-_Nevermind_-_10_-_Stay_Away_(Audio)_(320kbps).mp3" },
+                                new Track { Title = "On a Plain", DurationSecs = 196, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Nirvana_-_Nevermind_-_11_-_On_A_Plain_(Live_On_MTV_Unplugged,_1993___Unedited)_(320kbps).mp3" },
+                                new Track { Title = "Something in the Way", DurationSecs = 232, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Nirvana_-_Nevermind_-_12_-_Something_In_The_Way_(Audio)_(320kbps).mp3" },
+                            }
+                        }
+                    }
+                },
+                new Artist  // Prodigy
+                {
+                    Name = "Prodigy", Country = Country.United_Kingdom, CareerStartDate = new DateTime(1990, 1, 1), PhotoUrl = "", ArtistGenres = new List<Genre> { Genre.Electronic },
+                    Albums = new List<Album> {
+                        new Album {
+                            Title = "The Fat of the Land", ReleaseDate = new DateTime(1997, 1, 1), CoverPhotoUrl = "https://upload.wikimedia.org/wikipedia/en/3/3b/TheProdigy-TheFatOfTheLand.jpg?20171231004557",
+                            Tracks = new List<Track> {
+                                new Track { Title = "Smack My Bitch Up", DurationSecs = 342, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Prodigy_-_The_Fat_of_the_Land_-_1_-_Smack_My_Bitch_Up_(320kbps).mp3" },
+                                new Track { Title = "Breathe", DurationSecs = 239, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Prodigy_-_The_Fat_of_the_Land_-_2_-_Breathe_(Official_Video)_(320kbps).mp3" },
+                                new Track { Title = "Diesel Power", DurationSecs = 257, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Prodigy_-_The_Fat_of_the_Land_-_3_-_Diesel_Power_(320kbps).mp3" },
+                                new Track { Title = "Funky Shit", DurationSecs = 316, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Prodigy_-_The_Fat_of_the_Land_-_4_-_Funky_Shit_(320kbps).mp3" },
+                                new Track { Title = "Serial Thrilla", DurationSecs = 311, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Prodigy_-_The_Fat_of_the_Land_-_5_-_Serial_Thrilla_(320kbps).mp3" },
+                                new Track { Title = "Mindfields", DurationSecs = 404, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Prodigy_-_The_Fat_of_the_Land_-_6_-_Mindfields_(Live_in_Russia)_(320kbps).mp3" },
+                                new Track { Title = "Narayan", DurationSecs = 393, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Prodigy_-_The_Fat_of_the_Land_-_7_-_Narayan_(320kbps).mp3" },
+                                new Track { Title = "Firestarter", DurationSecs = 226, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Prodigy_-_The_Fat_of_the_Land_-_8_-_Firestarter_(Official_Video)_(320kbps).mp3" },
+                                new Track { Title = "Climbatize", DurationSecs = 397, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Prodigy_-_The_Fat_of_the_Land_-_9_-_Climbatize_(320kbps).mp3" },
+                                new Track { Title = "Fuel My Fire", DurationSecs = 258, TrackGenres = new List<Genre> { Genre.Rock }, AudioUrl = "Prodigy_-_The_Fat_of_the_Land_-_10_-_Fuel_My_Fire_(320kbps).mp3" },
+                            }
+                        }
+                    }
+                }
+            };
+
+            context.Artists.AddOrUpdate(artists.ToArray());
+            context.Albums.AddOrUpdate(artists.SelectMany(o => o.Albums).ToArray());
+            context.Tracks.AddOrUpdate(artists.SelectMany(o => o.Albums).SelectMany(o => o.Tracks).ToArray());
+
+            context.SaveChanges();
+
             base.Seed(context);
+
         }
     }
 }
--- Experiments/Program.cs
+++ Experiments/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL;
 using Entities.Enums;
 
 
@@ -12,8 +14,35 @@ namespace Experiments
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
 
+
+            Console.WriteLine("Experiments Start");
+
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            var artists = db.Artists
+                .Include("Albums")
+                .Include("Albums.Tracks")
+                .ToList();
+
+            foreach (var artist in artists)
+            {
+                Console.WriteLine(artist.Name);
+
+                foreach(var album in artist.Albums)
+                {
+                    Console.WriteLine($"\t{album.Title}");
+
+                    foreach(var track in album.Tracks)
+                    {
+                        Console.WriteLine($"\t\t{track.Title}");
+                    }
+                }
+            }
+
+
+            Console.WriteLine("Experiments End");
+            Console.ReadKey();
 
         }
     }
