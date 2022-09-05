efd16b7f3f95c39a71551ec02bac396b86929482
kiriakos <kiris3992@gmail.com>
2022-09-06 00:42:00 +0300

    Team Seeder

---
+++ DAL/Initializers/TeamsSeeding/Seed_Panos.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Enums;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Initializers.TeamsSeeding
+{
+    public class Seed_Panos : AbsTeamSeeder, ITeamSeeder
+    {
+        public List<Artist> GetArtists()
+        {
+            return null;
+        }
+    }
+}
