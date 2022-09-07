87451441008b1376f71f11d087029c6eb713311b
kiriakos <kiris3992@gmail.com>
2022-09-08 00:59:44 +0300

    TeamSeedding Refactor

--- DAL/Initializers/MockUpDbInitializer.cs
+++ DAL/Initializers/MockUpDbInitializer.cs
@@ -1,13 +1,8 @@
 ﻿using DAL.Initializers.TeamsSeeding;
-using Entities.Enums;
-using Entities.Models;
 using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Migrations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace DAL.Initializers
 {
@@ -15,48 +10,18 @@ namespace DAL.Initializers
     {
         protected override void Seed(ApplicationDbContext context)
         {
-            Genre blues = new Genre("Blues");
-            Genre chillstep = new Genre("Chillstep");
-            Genre classical = new Genre("Classical");
-            Genre country = new Genre("Country");
-            Genre dance = new Genre("Dance");
-            Genre disco = new Genre("Disco");
-            Genre electronic = new Genre("Electronic");
-            Genre folk = new Genre("Folk");
-            Genre hipHop = new Genre("Hip Hop");
-            Genre house = new Genre("House");
-            Genre instrumental = new Genre("Instrumental");
-            Genre jazz = new Genre("Jazz");
-            Genre kid = new Genre("Kid");
-            Genre latin = new Genre("Latin");
-            Genre metal = new Genre("Metal");
-            Genre opera = new Genre("Opera");
-            Genre pop = new Genre("Pop");
-            Genre progressive = new Genre("Progressive");
-            Genre punk = new Genre("Punk");
-            Genre rap = new Genre("Rap");
-            Genre reggae = new Genre("Reggae");
-            Genre rnb = new Genre("Rnb");
-            Genre rock = new Genre("Rock");
-            Genre soul = new Genre("Soul");
-            Genre techno = new Genre("Techno");
-            Genre traditional = new Genre("Traditional");
-            Genre trance = new Genre("Trance");
-            Genre trap = new Genre("Trap");
-            Genre hardRock = new Genre("Hard Rock");
-            Genre heavyMetal = new Genre("Heavy Metal");
-            Genre grunge = new Genre("Grunge");
+            var teamSeeder = new TeamSeeder().GetSeed();
 
-            context.Genres.AddOrUpdate(blues, chillstep, classical, country, dance, disco, electronic, folk, hipHop, house, instrumental, jazz, kid, latin, metal, opera, pop, progressive, punk, rap, reggae, rnb, rock, soul, techno, traditional, trance, trap, hardRock, heavyMetal, grunge);
-
-            List<ITeamSeeder> teamSeeders = new List<ITeamSeeder> { new Seed_Aggelos(), new Seed_Kiris(), new Seed_Orestis(), new Seed_Panos(), new Seed_Spyros() };
-            foreach(var seeder in teamSeeders)
+            context.Genres.AddOrUpdate(teamSeeder.genres.ToArray());
+            foreach(var seeder in teamSeeder.seeders)
             {
-                var artists = seeder.GetArtists();
-
-                if (artists != null)
+                try
                 {
-                    context.Artists.AddOrUpdate(artists.ToArray());
+                    context.Artists.AddOrUpdate(seeder.GetArtists().ToArray());
+                }
+                catch(Exception ex)
+                {
+                    Debug.WriteLine($"{seeder.GetType().Name} : {ex.Message}");
                 }
             }
 
---
+++ DAL/Initializers/TeamsSeeding/TeamSeeder.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Initializers.TeamsSeeding
+{
+    public class TeamSeeder
+    {
+        public (List<ITeamSeeder> seeders, List<Genre> genres) GetSeed()
+        {
+            List<ITeamSeeder> teamSeeders = new List<ITeamSeeder> { new Seed_Aggelos(), new Seed_Kiris(), new Seed_Orestis(), new Seed_Panos(), new Seed_Spyros() };
+
+            var bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public;
+            var fieldValues = teamSeeders[0].GetType()
+                                 .GetFields(bindingFlags)
+                                 .Where(field => field.FieldType.Name == "Genre")
+                                 .Select(o => (Genre)o.GetValue(teamSeeders[0]))
+                                 .ToList();
+
+            return (teamSeeders, fieldValues);
+        }
+    }
+}
