912e409840d7fe92a7f8507979ce9460404dd7b3
Panagiotis Chnarakis <panagiotis.chnarakis@Peoplecert.onmicrosoft.com>
2022-09-07 18:04:33 +0300

    trackapiA

---
+++ MusicMonkeyWebApp/Controllers/ApiControllers/TrackApiController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using DAL;
+using Entities.Models;
+
+namespace MusicMonkeyWebApp.Controllers.ApiControllers
+{
+    public class TrackApiController : BaseApiController
+    {
+        
+
+        // GET: api/Track
+        public IEnumerable<Object> GetTracks()
+        {
+            return MapTracksToDTO();
+        }
+
+        private IEnumerable<Object> MapTracksToDTO()
+        {
+            var tracks = unit.Tracks.GetTracksWithEverything();
+            var tracksDTO = tracks.Select(i =>
+                 new
+                 { // Tracks
+                     Title = i.Title,
+                     DurationSecs = i.DurationSecs,
+                     AudioUrl = i.AudioUrl,
+                     Popularity = i.Popularity,
+                     TrackGenres = i.TrackGenres.SelectMany(p => new string[] { p.Type })
+                 }
+                );
+            return tracksDTO;
+        }
+
+        // GET: api/Track/5
+        [ResponseType(typeof(Track))]
+        public IHttpActionResult GetTrack(int id)
+        {
+            Track track = db.Tracks.Find(id);
+            if (track == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(track);
+        }
+
+        // PUT: api/Track/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTrack(int id, Track track)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != track.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(track).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TrackExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Track
+        [ResponseType(typeof(Track))]
+        public IHttpActionResult PostTrack(Track track)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Tracks.Add(track);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = track.Id }, track);
+        }
+
+        // DELETE: api/Track/5
+        [ResponseType(typeof(Track))]
+        public IHttpActionResult DeleteTrack(int id)
+        {
+            Track track = db.Tracks.Find(id);
+            if (track == null)
+            {
+                return NotFound();
+            }
+
+            db.Tracks.Remove(track);
+            db.SaveChanges();
+
+            return Ok(track);
+        }
+
+      
+
+        private bool TrackExists(int id)
+        {
+            return db.Tracks.Count(e => e.Id == id) > 0;
+        }
+    }
+}
