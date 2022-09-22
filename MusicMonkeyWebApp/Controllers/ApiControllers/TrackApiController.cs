0b2aa4f621b798f9d42f0f7622f2cc1daee2401f
Orestis Kefis <raven1996@hotmail.gr>
2022-09-22 22:30:14 +0300

    Chart Changes

--- MusicMonkeyWebApp/Controllers/ApiControllers/TrackApiController.cs
+++ MusicMonkeyWebApp/Controllers/ApiControllers/TrackApiController.cs
@@ -19,11 +19,14 @@ namespace MusicMonkeyWebApp.Controllers.ApiControllers
         {
             IEnumerable<Track> tracks = unit.Tracks.GetTracksWithEverything();
             IEnumerable<object> trackDtoModels = new List<object>();
-            
+
             switch (type)
             {
                 case "full":
                     trackDtoModels = tracks.Select(x => FullTrackDTOModel(x));
+                    break;
+                case "first10":
+                    trackDtoModels = tracks.Select(x => FullTrackDTOModel(x)).Take(10);
                     break;
                 default:
                     trackDtoModels = tracks.Select(x => PartialTrackDTOModel(x));
@@ -172,6 +175,36 @@ namespace MusicMonkeyWebApp.Controllers.ApiControllers
         //Custom Service Methods
         private object FullTrackDTOModel(Track track)
         {
+            return new  //Tracks
+            {
+                track.Id,
+                track.Title,
+                track.DurationSecs,
+                track.AudioUrl,
+                track.Popularity,
+                TrackGenres = track.TrackGenres.SelectMany(p => new string[] { p.Type }),
+                Album = new  //Album
+                {
+                    track.Album.Id,
+                    track.Album.Title,
+                    track.Album.ReleaseDate,
+                    track.Album.CoverPhotoUrl,
+                    AlbumGenres = track.Album.AlbumGenres.SelectMany(p => new string[] { p.Type }),
+                    Artist = new  //Artist
+                    {
+                        track.Album.Artist.Name,
+                        track.Album.Artist.Country,
+                        track.Album.Artist.PhotoUrl,
+                        track.Album.Artist.CareerStartDate,
+                        ArtistGenres = track.Album.Artist.ArtistGenres.SelectMany(p => new string[] { p.Type })
+                    }
+                }
+            };
+        }
+        private object RandomTrackDTOModel(int id)
+        {
+            Track track = unit.Tracks.GetTrackByIdWithEverything(id);
+
             return new  //Tracks
             {
                 track.Id,
